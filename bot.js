47e74bb7fae1ae1675dc6017711f292e9dc9b1dd
ERROR X PREDATOR <86637047+Alinshan@users.noreply.github.com>
2022-03-11 11:19:32 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -140,7 +140,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
         });
 
         console.log(
-            chalk.green.bold('✅️RUDHRA working ' + config.WORKTYPE + ' 𝚗𝚘𝚠 '));
+            chalk.green.bold('✅️Toxic Alexa working ' + config.WORKTYPE + ' 𝚗𝚘𝚠 '));
           // thanks to afnanplk
 	    if (config.LANG == 'EN' || config.LANG == 'ML') {
                 await git.fetch();
@@ -339,8 +339,8 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
                         } catch (error) {
                             if (config.NOLOG == 'off') {
                                 
-                                await conn.sendMessage(conn.user.jid, '*~_________~ ༄ƦUÐҤƦλ࿐ ~______~*' +
-                                    '\n\n*😵 ' + error + '*\n\n```Report errors\njoin ⚠️Warning bot not allowed in the group\nchat.whatsapp.com/HVpTaTICeUi2G7hPlUlGUP``` ' 
+                                await conn.sendMessage(conn.user.jid, '*~_________~ ༄ALEXA࿐ ~______~*' +
+                                    '\n\n*😵 ' + error + '*\n\n```Report errors in the group\njoin \nhttps://chat.whatsapp.com/KTcvOHqo3Kl1YxGpIdRjif``` ' 
                                     , MessageType.text);
                             }
                         }
